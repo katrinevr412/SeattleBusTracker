b0a03c06e07196d6b79eb9736dacb7ae7026b062
Yang <yufayang@amazon.com>
2018-09-17 13:09:13 -0700

    update android branch, add iterative query mode

--- main.py
+++ main.py
@@ -4,17 +4,22 @@ import re
 
 
 def main():
-    cmd_str = raw_input("Input the bases you want to track (c, n, e, s), or routes separated by space:")
     tracker = Tracker()
-    if re.match('^[cnes]+$', cmd_str):
-        tracker.track_missing_vehicle(cmd_str=cmd_str)
-    else:
-        tracker.track_by_route(
-            filter(
-                lambda route_id: route_id.isdigit() and int(route_id) in Constants.ALL_LINES,
-                cmd_str.split(' ')
+    while True:
+        cmd_str = raw_input("Input the bases you want to track (c, n, e, s), or routes separated by space. Enter q to exit.")
+        if re.match('^[cnes]+$', cmd_str):
+            tracker.track_missing_vehicle(cmd_str=cmd_str)
+        elif re.match('^[0-9 ]+$', cmd_str):
+            tracker.track_by_route(
+                filter(
+                    lambda route_id: route_id.isdigit() and int(route_id) in Constants.ALL_LINES,
+                    cmd_str.split(' ')
+                )
             )
-        )
+        elif cmd_str.strip() in ['q', 'exit']:
+            break
+        else:
+            print 'Input is invalid, please try again.'
 
 
 if __name__ == '__main__':
